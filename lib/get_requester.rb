faab491d246a5a0a7b4cb4db95ac7e817b1b7654
Ryan McCahon <mccahonr@yahoo.com>
2020-05-22 23:58:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,12 +5,4 @@ require 'json'
 
 class GetRequester(URL)
 
-  URL = ""
-
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
 end
